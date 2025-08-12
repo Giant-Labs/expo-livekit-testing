7e2fd494874e768f159a07c91d9f2b03e9adf156
Jens Andersson <jens@fritan.com>
2025-08-12 12:56:27 +0200

    Basic setup for testin

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,29 +1,147 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import { useConnectionState, useRoomContext } from "@livekit/components-react";
+import {
+  AudioSession,
+  LiveKitRoom,
+  setLogLevel,
+  useIOSAudioManagement,
+  useVoiceAssistant,
+} from "@livekit/react-native";
+import { registerGlobals } from "@livekit/react-native-webrtc";
+import { setAudioModeAsync, useAudioPlayer } from "expo-audio";
+import { LogLevel } from "livekit-client";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import { Button, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+registerGlobals();
 
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
+const audioSource = require("@/assets/audio/ringing.m4a");
+setLogLevel(LogLevel.warn);
 
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
+function LiveKitRoomWrapper({
+  serverUrl,
+  token,
+}: {
+  serverUrl: string;
+  token: string;
+}) {
+  useEffect(() => {
+    console.log("starting audio session");
+    const start = async () => {
+      await AudioSession.configureAudio({
+        ios: {
+          defaultOutput: "speaker",
+        },
+      });
+      await AudioSession.startAudioSession();
+    };
+
+    start();
+
+    return () => {
+      AudioSession.stopAudioSession();
+    };
+  }, []);
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <LiveKitRoom
+      serverUrl={serverUrl}
+      token={token}
+      connect={true}
+      audio={true}
+      video={false}
+      onError={(error) => {
+        console.error("LiveKitRoom error", error);
+      }}
+      onMediaDeviceFailure={(failure) => {
+        console.error("LiveKitRoom media device failure", failure);
+      }}
+      onEncryptionError={(error) => {
+        console.error("LiveKitRoom encryption error", error);
+      }}
+    >
+      <RoomView />
+    </LiveKitRoom>
+  );
+}
+export const RoomView = () => {
+  const { agentTranscriptions, state } = useVoiceAssistant();
+
+  const room = useRoomContext();
+  useIOSAudioManagement(room, true);
+
+  const messages = useMemo(
+    () => agentTranscriptions.map((t) => t.text),
+    [agentTranscriptions]
+  );
+
+  const connectionState = useConnectionState();
+
+  return (
+    <View>
+      {messages.map((message) => (
+        <Text key={message}>{message}</Text>
+      ))}
+      <Text>State: {state}</Text>
+      <Text>Connection State: {connectionState}</Text>
+    </View>
+  );
+};
+
+export default function App() {
+  const [credentials, setCredentials] = useState<{
+    serverUrl: string;
+    token: string;
+  }>();
+
+  const [loading, setLoading] = useState(false);
+
+  const ringingPlayer = useAudioPlayer(audioSource);
+  useEffect(() => {
+    ringingPlayer.loop = true;
+
+    setAudioModeAsync({
+      playsInSilentMode: true,
+      interruptionMode: "mixWithOthers",
+    });
+  }, [ringingPlayer]);
+
+  const fetchCredentials = useCallback(async () => {
+    ringingPlayer.play();
+    setLoading(true);
+    const res = await fetch(`http://localhost:3000/api/app/get-testing-token`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+    });
+    const data = await res.json();
+    setCredentials(data);
+    setLoading(false);
+    setTimeout(() => {
+      ringingPlayer.pause();
+    }, 3000);
+  }, [ringingPlayer]);
+
+  const onEndPress = () => {
+    setCredentials(undefined);
+  };
+
+  return (
+    <SafeAreaView>
+      {loading ? (
+        <Text>Getting credentials... (calling)</Text>
+      ) : !credentials ? (
+        <Text>No credentials</Text>
+      ) : (
+        <LiveKitRoomWrapper
+          serverUrl={credentials?.serverUrl}
+          token={credentials?.token}
+        />
+      )}
+      {credentials ? (
+        <Button title="End Call" onPress={onEndPress} />
+      ) : (
+        <Button title="Start Call" onPress={fetchCredentials} />
+      )}
+    </SafeAreaView>
   );
 }
